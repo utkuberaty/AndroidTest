ec3301cae41f9058847dd0905f2f0f1bf76b7a47
utkuyildiz <utku.yildiz@path.com.tr>
2022-04-14 17:16:00 +0300

    change flow with coroutine and use original text for process instead example

--- app/src/main/java/com/eightbars/test/MainActivity.kt
+++ app/src/main/java/com/eightbars/test/MainActivity.kt
@@ -3,15 +3,18 @@ package com.eightbars.test
 import android.content.Intent
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 
 
-class MainActivity : AppCompatActivity(), WordCounter  {
+class MainActivity : AppCompatActivity(), WordCounter {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +23,29 @@ class MainActivity : AppCompatActivity(), WordCounter  {
         val textView = findViewById<TextView>(R.id.textView)
 
         button.setOnClickListener {
-           countWords(textView.text.toString()) {  startNext(it.toCountResult()) }
+//            countWords(textView.text.toString()) { startNext(it.toCountResult()) }
+            lifecycleScope.launch {
+                /*val exampleReal = mutableMapOf<String, Int>()
+                val exampleList = multiThreadedCountWorks(textView.text.toString()).toList()
+
+                exampleList.forEach {
+                    it.keys.forEach { key ->
+                        val currentSize = exampleReal[key] ?: 0
+                        exampleReal[key] = currentSize + it[key]!!
+                    }
+                }*/
+
+                /* multiThreadedCountWorks(textView.text.toString()).collect {
+                     startNext(it.toCountResult())
+                     Log.i("Example", "${it.keys} ${it.values}")
+                 }*/
+                /*countWordsWithFlow(textView.text.toString()).collectLatest {
+                    startNext(it.toCountResult())
+                }*/
+
+                val countWorlds = multiThreadedCountWorks(textView.text.toString())
+                startNext(countWorlds.toCountResult())
+            }
         }
     }
 
--- app/src/main/java/com/eightbars/test/WordCounter.kt
+++ app/src/main/java/com/eightbars/test/WordCounter.kt
@@ -1,13 +1,12 @@
 package com.eightbars.test
 
 import android.os.Parcelable
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
 import java.util.*
@@ -24,10 +23,15 @@ interface WordCounter {
         completion(input.countWords())
     }
 
+    fun countWordsWithFlow(input: String) = flow {
+        // default implementation counts words on single thread
+        emit(input.countWords())
+    }.flowOn(Dispatchers.IO)
+
     /**
      * implementation should count words on a single thread and callback on main Thread, using coroutines
      */
-    suspend fun singleThreadedCountWords(input: String) : Map<String, Int> = emptyMap()
+    suspend fun singleThreadedCountWords(input: String): Map<String, Int> = emptyMap()
 
     /**
      * implementation should count words on a single thread per line and callback on main Thread, using coroutines
@@ -36,12 +40,26 @@ interface WordCounter {
      * - how may parts should I split the string into?
      * - how do you split the string?
      */
-    suspend fun multiThreadedCountWorks(input: String) : Map<String, Int> = emptyMap()
+    suspend fun multiThreadedCountWorks(input: String): Map<String, Int> {
+        val exampleReal = mutableMapOf<String, Int>()
+        val listOfCountWords = mutableListOf<Map<String, Int>>()
+        input.chunked(input.length / 2).forEach {
+            listOfCountWords.add(it.countWords())
+        }
+        listOfCountWords.forEach {
+            it.keys.forEach { key ->
+                val currentSize = exampleReal[key] ?: 0
+                exampleReal[key] = currentSize + (it[key] ?: 0)
+            }
+        }
+        return exampleReal
+    }
+
 
     /**
      * Create a Flow that emits the counts for each row
      */
-    suspend fun wordsOnLine(input: String) : Flow<Map<String, Int>> = emptyFlow()
+    suspend fun wordsOnLine(input: String): Flow<Map<String, Int>> = emptyFlow()
 }
 
 @Parcelize
@@ -53,18 +71,18 @@ data class WordCount(val string: String, val count: Int) : Parcelable
 /**
  * Utility function to count words in text.
  */
-fun String.countWords() : Map<String, Int> {
+fun String.countWords(): Map<String, Int> {
     // how would you sanitise the string? are there any problems here
     return split(" ")
-        .map {  it }
+        .map { it.lowercase() }
         .groupBy { it }
         .mapValues { it.value.size }
 }
 
-fun String.splitString() : List<String> {
+fun String.splitString(): List<String> {
     // how would you sanitise the string? are there any problems here
     return lines()
 }
 
-fun Map<String, Int>.toCountResult() : CountResult =
+fun Map<String, Int>.toCountResult(): CountResult =
     CountResult(this.entries.map { WordCount(it.key, it.value) })
